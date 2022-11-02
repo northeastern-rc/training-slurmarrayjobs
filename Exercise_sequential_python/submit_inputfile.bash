6cb2767274a14b84d0bdad3d736fd07079b87428
manasvita <github@manasvita.com>
2022-11-01 22:14:26 -0400

    Moved the textual comment about partition in #SBATCH directives section

--- Exercise_sequential_python/submit_inputfile.bash
+++ Exercise_sequential_python/submit_inputfile.bash
@@ -1,15 +1,12 @@
 #!/bin/bash
 #SBATCH -J getMax 		##set job name 
-#SBATCH --partition=reservation                                                                                                                                                                                    
+#SBATCH --partition=reservation # Use a regular partition such as "express" when not in training & remove --reservation 
 #SBATCH --reservation=fall_training_cpu_2022
 #SBATCH --nodes=1		#set node number to 1
 #SBATCH --ntasks=1		#set number of tasks (CPUs) to 1
 #SBATCH --array=1-4%4 		#create 4 array jobs, run all 4 at a time.
 #SBATCH --output=%A-%a.out	#set output filename with main job ID and task array ID
 #SBATCH --error=%A-%a.err	#set error filename with main job ID and task array ID
-
-# Use a regular partition, such as debug, when not in a training
-##SBATCH --partition=debug       #set partition name to "debug" (very short runs) 
 
 # Purge any existing modules on the compute node to get a clean
 # environment for running your program:
